9cdb710219e7c7b45debd9c3f6c9e76d8370730a
Nicholas Elliott <nick@nickelliott.us>
2013-04-01 12:01:27 -0300

    Update MainActivity.java

--- src/com/seminolestate/congressfinder/MainActivity.java
+++ src/com/seminolestate/congressfinder/MainActivity.java
@@ -4,23 +4,44 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+	private final int GROUP_DEFAULT = 0;
+	private final int MENU_PREF = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		Intent i = new Intent(this, MyLocation.class);
-		startActivity(i);
+		//Intent i = new Intent(this, MyLocation.class);
+		//startActivity(i);
 	}
-
+	/*
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	*/
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+		menu.add(GROUP_DEFAULT, MENU_PREF, 0, "Location services");
+		
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemID()) {
+			case MENU_PREF:
+				Intent i = new Intent(this, MyLocation.class)
+				startActivity(i);
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
 }
