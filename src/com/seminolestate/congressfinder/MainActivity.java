262a8a9cdb0db8db329f72233a6df3a0fc4e6575
Nicholas Elliott <nick@nickelliott.us>
2013-04-01 22:21:28 -0400

    Updated location demo and standardized menu setting

--- src/com/seminolestate/congressfinder/MainActivity.java
+++ src/com/seminolestate/congressfinder/MainActivity.java
@@ -4,11 +4,12 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-	private final int GROUP_DEFAULT = 0;
-	private final int MENU_PREF = 1;
+	//private final int GROUP_DEFAULT = 0;
+	//private final int MENU_PREF = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,26 +24,19 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		MenuInflater inflater = getMenuInflater();
-		getMenuInflater().inflate(R.menu.main, menu);
+		inflater.inflate(R.menu.my_location, menu);
 		return true;
 	}
-	/*
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu){
-		menu.add(GROUP_DEFAULT, MENU_PREF, 0, "Location services");
-		
-		return super.onCreateOptionsMenu(menu);
-	}
-	*/
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		switch(item.getItemID()) {
-			case R.id.location_services:
-				Intent i = new Intent(this, MyLocation.class)
-				startActivity(i);
-				return true;
+		switch (item.getItemId()) {
+		case R.id.location_services:
+			Intent i = new Intent(this, MyLocation.class);
+			startActivity(i);
+			return true;
+			
+		default: return super.onOptionsItemSelected(item);
 		}
-		return super.onOptionsItemSelected(item);
 	}
-	
 }
