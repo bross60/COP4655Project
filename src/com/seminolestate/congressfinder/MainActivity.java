5d7e75238c9031c3c87bee357457f9bc89c3705a
Nicholas Elliott <nick@nickelliott.us>
2013-04-01 22:25:27 -0400

    Location Services demo working - Cleaned up code a little

--- src/com/seminolestate/congressfinder/MainActivity.java
+++ src/com/seminolestate/congressfinder/MainActivity.java
@@ -8,21 +8,15 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-	//private final int GROUP_DEFAULT = 0;
-	//private final int MENU_PREF = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		//Intent i = new Intent(this, MyLocation.class);
-		//startActivity(i);
 	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.my_location, menu);
 		return true;
